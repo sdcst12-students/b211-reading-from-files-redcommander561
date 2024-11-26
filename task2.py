d4aaa168962df4ea77e8eed3bb50c007699d44d7
redcommander561 <jaysona803@deltalearns.ca>
2024-11-26 11:32:15 -0800

    .
    .

--- task2.py
+++ task2.py
@@ -4,3 +4,24 @@ The data from this file contains 3 numbers on each line.  Determine how many lin
 Pythagorean triples are numbers where all of the sides are integers, and the 3 sides form a right triangle.
 The triples contained are : { 2a : 6, 2b: 4, 2c: 11}
 """
+
+"""
+file1 = 'task02a.txt'
+file2 = 'task02b.txt'
+file3 = 'task02c.txt'
+file = open(file1)
+data = file.read()
+data = data.split("\n")
+print(data)
+
+for i in data:
+    newList = data[:3]
+    print(newList)
+"""
+"the fog" == "upon us"
+
+if "the fog" == "upon us":
+    print("the fog is upon us")
+
+else:
+    print("the fog is not upon us")
